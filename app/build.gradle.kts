5f95a33b1961ed62ec0201388a118f26114793dd
mpilasy <mpilasy@gmail.com>
2026-02-26 21:06:28 -0600

    bump version to 1.0.2 with changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "orinasa.njarasoa.maripanatokana"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
