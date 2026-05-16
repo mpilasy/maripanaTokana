fe0ef710f200c1ec4db08cd8837b28359ea2f7f1
mpilasy <mpilasy@duck.com>
2026-05-15 21:53:29 -0500

    Bump version to 1.0.19 and clean metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "orinasa.njarasoa.maripanatokana"
         minSdk = 24
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.0.18"
+        versionCode = 20
+        versionName = "1.0.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
