8447ba3450396235b31fb8b5037ba8684bde6fe3
mpilasy <mpilasy@duck.com>
2026-05-15 22:31:32 -0500

    Bump version to 1.0.23 for a truly fresh start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "orinasa.njarasoa.maripanatokana"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.0.22"
+        versionCode = 24
+        versionName = "1.0.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
