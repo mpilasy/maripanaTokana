4baab5f20be0e6fe027fc9610b99ce5b65f52474
mpilasy <mpilasy@duck.com>
2026-07-09 19:07:01 -0500

    Bump web version display to 1.2.2 to match Android

--- web/src/lib/components/Footer.svelte
+++ web/src/lib/components/Footer.svelte
@@ -21,7 +21,7 @@
 			{$_('credits_weather_data')}
 			<a href={SOURCE_INFO[$weatherSource].url} target="_blank" rel="noopener">{SOURCE_INFO[$weatherSource].label}</a>
 		</span>
-		<span class="version">v1.2.1</span>
+		<span class="version">v1.2.2</span>
 	</div>
 </footer>
 
